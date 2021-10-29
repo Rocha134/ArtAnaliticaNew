33ece130215beef3eeda2d527ee06ba369fb2d03
Rocha134 <A01730560@tec.mx>
2021-10-29 14:25:40 -0500

    final boxplot

--- kmeans_sklearn.py
+++ kmeans_sklearn.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from kneed import KneeLocator
 
-df = pd.read_csv("Mall_Customers.csv")
+df = pd.read_csv("ulabox_orders_with_categories_partials_2017.csv")
 
-dfp = df[["Annual_Income_(k$)", "Spending_Score"]]
+dfp = df[["Food%", "Fresh%", "Drinks%", "Home%", "Beauty%", "Health%", "Baby%", "Pets%"]]
 
 ssd = []
 ks = range(1,11)
@@ -23,13 +23,39 @@ k = round(kneedle.knee)
 
 print(f"Number of clusters suggested by knee method: {k}")
 
-kmeans = KMeans(n_clusters=k).fit(df[["Annual_Income_(k$)", "Spending_Score"]])
-sns.scatterplot(data=df, x="Annual_Income_(k$)", y="Spending_Score", hue=kmeans.labels_)
+kmeans = KMeans(n_clusters=k).fit(df[["Food%", "Fresh%", "Drinks%", "Home%", "Beauty%", "Health%", "Baby%", "Pets%"]])
+sns.scatterplot(data=df, x="weekday", y="hour", hue=kmeans.labels_)
 plt.show()
 
+cluster0=df[kmeans.labels_==0]
+print(cluster0.describe())
 
-from sklearn.tree import DecisionTreeClassifier, export_text
+cluster1=df[kmeans.labels_==1]
+print(cluster1.describe())
 
-tree = DecisionTreeClassifier()
-tree.fit(df[["Age", "Annual_Income_(k$)", "Spending_Score"]], kmeans.labels_)
-print(export_text(tree, feature_names=["Age", "Annual_Income_(k$)", "Spending_Score"]))
+cluster2=df[kmeans.labels_==2]
+print(cluster2.describe())
+
+cluster3=df[kmeans.labels_==3]
+print(cluster3.describe())
+
+cluster4=df[kmeans.labels_==4]
+print(cluster4.describe())
+# %%
+sns.boxplot(data=cluster0[['Food%','Fresh%','Drinks%','Home%','Beauty%','Health%','Baby%','Pets%']])
+sns.boxplot(data=cluster1[['Food%','Fresh%','Drinks%','Home%','Beauty%','Health%','Baby%','Pets%']])
+sns.boxplot(data=cluster2[['Food%','Fresh%','Drinks%','Home%','Beauty%','Health%','Baby%','Pets%']])
+sns.boxplot(data=cluster3[['Food%','Fresh%','Drinks%','Home%','Beauty%','Health%','Baby%','Pets%']])
+sns.boxplot(data=cluster4[['Food%','Fresh%','Drinks%','Home%','Beauty%','Health%','Baby%','Pets%']])
+# %%
+sns.histplot(data=cluster0, x= 'weekday')
+sns.histplot(data=cluster1, x= 'weekday')
+sns.histplot(data=cluster2, x= 'weekday')
+sns.histplot(data=cluster3, x= 'weekday')
+sns.histplot(data=cluster4, x= 'weekday')
+# %%
+# from sklearn.tree import DecisionTreeClassifier, export_text
+#
+# tree = DecisionTreeClassifier()
+# tree.fit(df[["weekday", "hour", "discount%", "total_items"]], kmeans.labels_)
+# print(export_text(tree, feature_names=["weekday", "hour", "discount%", "total_items"]))
